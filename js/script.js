4fb32c17981fbd89e4e09cf80c084c7ed793b622
Twahidul Islam <twahidulislam2005@gmail.com>
2025-03-06 12:24:36 +0600

    add

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ $('.sliders').slick({
     nextArrow:'<i class="fa-solid fa-circle-chevron-right next"></i>'
   });
 // Slick Slider End 
+$('.serviceSlider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:false,
+});
+responsive: [
+  {
+    breakpoint: 575,
+    settings: {
+      arrows: false,
+      centerMode: true,
+      centerPadding: '40px',
+      slidesToShow: 1,
+    }
+  },
+  {
+    
+  }
+]
